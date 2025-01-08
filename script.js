0a9b54621233b28f224641836a4e5250681719bf
Jana Schueler <janaschueler@protonmail.com>
2025-01-08 13:53:53 +0100

    first handin

---
+++ script.js
@@ -0,0 +1,294 @@
+let backgroundColor;
+let typeName;
+let typeIcon;
+let indexPokemonMain;
+let currentIndex = 0;
+let cardsPerLoad = 20;
+let pokemon = [];
+let pokemonMain = [];
+let pokemonEvo = [];
+let pokemonList = [];
+let detailedpokemonMain = [];
+let evoChain = [];
+
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=500&offset=0";
+
+async function onloadFunc() {
+  showLoadingMessage();
+  disableLoadMoreButton();
+
+  await loadPokemonList();
+  await loadPokemonDetails();
+  await loadEvolutionChains();
+
+  currentIndex = 0;
+  renderCards();
+
+  hideLoadingMessage();
+  enableLoadMoreButton();
+
+  setupLoadMoreButton();
+}
+
+function showLoadingMessage() {
+  const loadingMessage = document.getElementById("loadingMessage");
+  loadingMessage.style.display = "flex";
+}
+
+function hideLoadingMessage() {
+  const loadingMessage = document.getElementById("loadingMessage");
+  loadingMessage.style.display = "none";
+}
+
+async function loadPokemonList() {
+  let pokemonListe = await getAllPokemon();
+  pokemon.push(...pokemonListe);
+}
+
+async function loadPokemonDetails() {
+  const detailsPromises = pokemon.map((p) => getPokemonDetails(p.url));
+  pokemonMain = await Promise.all(detailsPromises);
+}
+
+async function loadEvolutionChains() {
+  const evoPromises = pokemonMain.map(async (evo) => {
+    try {
+      const speciesData = await getPokemonDetails(evo.species.url);
+      return await getEvoChainDetails(speciesData.evolution_chain.url);
+    } catch (error) {
+      console.error("Fehler beim Abrufen der Evolution Chain:", error);
+      return [];
+    }
+  });
+  pokemonEvo = await Promise.all(evoPromises);
+}
+
+function setupLoadMoreButton() {
+  const loadMoreBtn = document.getElementById("loadMoreBtn");
+  loadMoreBtn.addEventListener("click", loadMoreCards);
+}
+
+async function getAllPokemon() {
+  let response = await fetch(BASE_URL);
+  try {
+    let responseToJson = await response.json();
+    return responseToJson.results;
+  } catch (error) {
+    console.error("Fehler beim Parsen der Antwort:", error);
+    return [];
+  }
+}
+
+async function getPokemonDetails(url) {
+  let response = await fetch(url);
+  return await response.json();
+}
+
+async function getEvoChainDetails(evolutionChainUrl) {
+  try {
+    let response = await fetch(evolutionChainUrl);
+    let evolutionData = await response.json();
+
+    let evolutions = [];
+    let currentChain = evolutionData.chain;
+
+    while (currentChain) {
+      evolutions.push(currentChain.species.name);
+      currentChain = currentChain.evolves_to[0];
+    }
+
+    return evolutions;
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Evolutionskette:", error);
+    return [];
+  }
+}
+
+function renderCards() {
+  let myCardRef = document.getElementById("cardContainer");
+  let pokemonToRender = pokemonMain.slice(currentIndex, currentIndex + cardsPerLoad);
+  currentIndex += cardsPerLoad;
+
+  pokemonToRender.forEach((pokemonList, index) => {
+    let backgroundColor = typeColors[pokemonList.types[0].type.name];
+    let typeIconsHTML = generateTypeIconsHTML(pokemonList, backgroundColor);
+    const modalId = `pokemonModal_${index + currentIndex - cardsPerLoad}`;
+    myCardRef.innerHTML += getCard(index + currentIndex - cardsPerLoad, pokemonList, backgroundColor, typeIconsHTML, modalId);
+  });
+}
+
+function generateTypeIconsHTML(pokemonList, backgroundColor) {
+  return pokemonList.types.map((type) => getIcon(backgroundColor, typeIcons[type.type.name])).join("");
+}
+
+function openModal(index) {
+  let pokemon = pokemonMain[index];
+  let backgroundColor = typeColors[pokemon.types[0].type.name];
+  let typeIconsHTML = "";
+
+  for (let typeIndex = 0; typeIndex < pokemon.types.length; typeIndex++) {
+    let typeName = pokemon.types[typeIndex].type.name;
+    let typeIcon = typeIcons[typeName];
+    typeIconsHTML += getIcon(backgroundColor, typeIcon);
+  }
+
+  renderModal(index, pokemon, backgroundColor, typeIconsHTML);
+  let modal = new bootstrap.Modal(document.getElementById("modalContainer"));
+  modal.show();
+}
+
+function renderModal(index, pokemon, backgroundColor, typeIconsHTML) {
+  let myModalRef = document.getElementById("pokemonModalBody");
+  myModalRef.innerHTML = "";
+
+  let abilityList = renderAbilities(pokemon);
+
+  myModalRef.innerHTML += getModal(pokemon, backgroundColor, abilityList, index);
+  renderAbilities(pokemon);
+  renderstats(pokemon);
+  renderEvo(index);
+}
+
+function renderAbilities(pokemon) {
+  let abilitiesPokemon = "";
+  for (let abilityIndex = 0; abilityIndex < pokemon.abilities.length; abilityIndex++) {
+    let abilityName = pokemon.abilities[abilityIndex].ability.name;
+    abilitiesPokemon += abilityName + ", ";
+  }
+  return abilitiesPokemon.slice(0, -2);
+}
+
+function renderstats(pokemon) {
+  let myStatsRef = document.getElementById("statsContainer");
+  myStatsRef.innerHTML = "";
+  let statsPokemon = "";
+
+  for (let statsIndex = 0; statsIndex < pokemon.stats.length; statsIndex++) {
+    let statsyName = pokemon.stats[statsIndex].stat.name;
+    let statsValue = pokemon.stats[statsIndex].base_stat;
+    statsPokemon += getStats(statsyName, statsValue);
+  }
+  myStatsRef.innerHTML = statsPokemon;
+}
+
+function getPokemonIdByName(name) {
+  let pokemon = pokemonMain.find((p) => p.name === name);
+  return pokemon ? pokemon.id : null;
+}
+
+function renderEvo(index) {
+  let myEvoRef = document.getElementById("evoContainer");
+  myEvoRef.innerHTML = "";
+
+  let evoChains = pokemonEvo[index];
+
+  for (let evoIndex = 0; evoIndex < evoChains.length; evoIndex++) {
+    let evoName = evoChains[evoIndex];
+    let pokemonId = getPokemonIdByName(evoName);
+    let evoImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonId}.png`;
+    myEvoRef.innerHTML += getEvo(evoName, evoImgUrl);
+  }
+}
+
+function loadMoreCards() {
+  disableLoadMoreButton();
+
+  let pokemonToLoad = pokemonMain.slice(currentIndex, currentIndex + cardsPerLoad);
+  renderCards(pokemonToLoad);
+  currentIndex += cardsPerLoad;
+
+  const lastCard = document.querySelector("#cardContainer").lastElementChild;
+  if (lastCard) {
+    lastCard.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+
+  enableLoadMoreButton();
+}
+
+document.getElementById("searchForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPokemon();
+});
+
+function disableLoadMoreButton() {
+  const loadMoreBtn = document.getElementById("loadMoreBtn");
+  loadMoreBtn.disabled = true;
+  loadMoreBtn.innerHTML = "Lädt...";
+}
+
+function enableLoadMoreButton() {
+  const loadMoreBtn = document.getElementById("loadMoreBtn");
+  loadMoreBtn.disabled = false;
+  loadMoreBtn.innerHTML = "Mehr laden";
+}
+
+document.getElementById("searchForm").addEventListener("submit", function (e) {
+  const searchInput = document.getElementById("searchInput");
+  const searchMessageContainer = document.getElementById("searchMessageContainer");
+
+  if (searchInput.value.length < 3) {
+    e.preventDefault();
+    searchMessageContainer.style.display = "block";
+  } else {
+    searchMessageContainer.style.display = "none";
+    searchPokemon(searchInput.value);
+  }
+});
+
+function searchPokemon(query) {
+  const cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = "";
+
+  const filteredPokemon = pokemonMain.filter((pokemon) => pokemon.name.toLowerCase().includes(query.toLowerCase()));
+
+  if (filteredPokemon.length > 0) {
+    filteredPokemon.forEach((pokemon, index) => {
+      const backgroundColor = typeColors[pokemon.types[0].type.name];
+      const typeIconsHTML = pokemon.types.map((type) => getIcon(backgroundColor, typeIcons[type.type.name])).join("");
+      const modalId = `pokemonModal_${index}`;
+      cardContainer.innerHTML += getCard(index, pokemon, backgroundColor, typeIconsHTML, modalId);
+    });
+  } else {
+    cardContainer.innerHTML = `<p class="text-center">No Pokémon found matching "${query}"</p>`;
+  }
+}
+
+document.getElementById("searchInput").addEventListener("input", function (event) {
+  if (event.target.value === "") {
+    resetSearch();
+  }
+});
+
+function resetSearch() {
+  const cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = "";
+
+  const allPokemonToRender = pokemonMain.slice(0, currentIndex);
+  allPokemonToRender.forEach((pokemon, index) => {
+    const backgroundColor = typeColors[pokemon.types[0].type.name];
+    const typeIconsHTML = pokemon.types.map((type) => getIcon(backgroundColor, typeIcons[type.type.name])).join("");
+
+    const modalId = `pokemonModal_${index}`;
+    cardContainer.innerHTML += getCard(index, pokemon, backgroundColor, typeIconsHTML, modalId);
+  });
+}
+
+function navigatePokemon(index) {
+  if (index < 0 || index >= pokemonMain.length) return;
+
+  const selectedPokemon = pokemonMain[index];
+  const backgroundColor = typeColors[selectedPokemon.types[0].type.name];
+  const abilityList = renderAbilities(selectedPokemon);
+
+  updateModalContent(selectedPokemon, backgroundColor, abilityList, index);
+}
+
+function updateModalContent(pokemon, backgroundColor, abilityList, index) {
+  const modalBody = getModal(pokemon, backgroundColor, abilityList, index);
+
+  const modalContainer = document.getElementById("pokemonModalBody");
+  modalContainer.innerHTML = modalBody;
+
+  renderstats(pokemon);
+  renderEvo(index);
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,19 +1,113 @@
-  // Get the scroll button:
-  let mybutton = document.getElementById("topBtn");
-  
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-  
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; 
-  }
-  
-  
+function getCard(indexPokemonMain, pokemonList, backgroundColor, typeIconsHTML) {
+  return `
+    <div class="col" onclick="openModal(${indexPokemonMain})">
+      <div class="card h-100">
+        <div class="card-head">
+          <small class="text-muted">${pokemonList.name}</small>
+        </div>
+        <img src="${pokemonList.sprites.other.home.front_default}" class="card-img-top" style="background-color:${backgroundColor}" alt="..." loading="lazy" />
+        <div class="card-footer">
+          ${typeIconsHTML}
+        </div>
+      </div>
+    </div>`;
+}
+
+function getIcon(backgroundColor, typeIcon) {
+  return `<div class="type-icon" style="background-color:${backgroundColor}">
+          <i class="${typeIcon}"></i>
+        </div>`;
+}
+
+function getModal(pokemon, backgroundColor, abilityList, index) {
+  return ` 
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="card-header position-relative" style="text-align: center">
+          <small class="text-muted">${pokemon.name}</small>
+          <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body d-flex justify-content-center align-items-center position-relative" style="background-color:${backgroundColor}">
+          <button class="btn btn-light position-absolute" style="left: 10px; top: 50%; transform: translateY(-50%);" id="prevButton" onclick="navigatePokemon(${index - 1})" ${index === 0 ? "disabled" : ""}>
+            <i class="bi bi-chevron-left"></i>
+          </button>
+          <img src="${pokemon.sprites.other.home.front_default}" class="card-img-top" alt="..." />
+          <button class="btn btn-light position-absolute" style="right: 10px; top: 50%; transform: translateY(-50%);" id="nextButton" onclick="navigatePokemon(${index + 1})" ${index === pokemonMain.length - 1 ? "disabled" : ""}>
+            <i class="bi bi-chevron-right"></i>
+          </button>
+        </div>
+        <ul class="nav nav-tabs w-100" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation" style="width: 33.33%">
+            <a class="nav-link active w-100 text-center" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Main</a>
+          </li>
+          <li class="nav-item" role="presentation" style="width: 33.33%">
+            <a class="nav-link w-100 text-center" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Stats</a>
+          </li>
+          <li class="nav-item" role="presentation" style="width: 33.33%">
+            <a class="nav-link w-100 text-center" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Evo Chain</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <table>
+              <tr>
+                <td>Height</td>
+                <td>: ${pokemon.height} cm</td>
+              </tr>
+              <tr>
+                <td>Weight</td>
+                <td>: ${pokemon.weight} g</td>
+              </tr>
+              <tr>
+                <td>Base experience</td>
+                <td>: ${pokemon.base_experience}</td>
+              </tr>
+              <tr>
+                <td>Abilities</td>
+                <td>: ${abilityList}</td>
+              </tr>
+            </table>
+          </div>
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <ul id="statsContainer" class="list-group">
+            </ul>
+          </div>
+          <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+            <div class="d-flex justify-content-center align-items-center mt-3">
+              <div class="mx-2 text-center">
+                <div class="row justify-content-center" id="evoContainer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function getStats(statsName, statsValue) {
+  return `
+    <li class="list-group-item">
+      <div class="d-flex justify-content-between align-items-center">
+        <span style="margin-right: 8px;">${statsName}</span>
+        <div class="progress" style="width: 70%; height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: ${statsValue}%;"
+               aria-valuenow="${statsValue}" aria-valuemin="0" aria-valuemax="100">
+            ${statsValue}
+          </div>
+        </div>
+      </div>
+    </li>
+  `;
+}
+
+function getEvo(evoName, evoImgUrl) {
+  return `
+    <div class="col-3 text-center mx-2">
+      <img src="${evoImgUrl}" class="img-fluid" alt="${evoName}" />
+      <figcaption style="color: white; font-size: 1.2rem;">${evoName}</figcaption>
+    </div>
+  `;
+}
